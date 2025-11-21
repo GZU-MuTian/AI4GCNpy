dbe7127d342051cbe01f72cf99f2943e04265a88
yuliu <yuliu@gzu.edu.cn>
2025-11-21 17:20:49 +0800

    test

--- src/ai4gcnpy/cli.py
+++ src/ai4gcnpy/cli.py
@@ -1,9 +1,26 @@
 import typer
+from rich.console import Console
+
+app = typer.Typer()
+console = Console()
+
+@app.command()
+def hello(
+    name: str = typer.Argument(..., help="Name to input CSV"),
+    formal: bool = False,
+    verbose: bool = typer.Option(False, "--verbose", "-v", help="Enable verbose mode"),
+):
+    if formal:
+        console.print(f"Good day, Ms. {name}.")
+    else:
+        console.print(f"Hello, {name}!")
 
 
-def main():
-    print("Hello World")
-
+@app.command()
+def graph_build(
+    verbose: bool = typer.Option(False, "--verbose", "-v", help="Enable verbose mode"),
+):
+    console.print(f"Building!")
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
